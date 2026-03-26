12d2d90113563e44d86e01f20ef95f26c8a444f9
Dan Welling <dwelling@umich.edu>
2026-03-26 19:26:28 -0400

    Changing event to battle creek.

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026miche",
+      "defaultValue": "2026mibat",
       "required": "true"
     },
     { "name": "Match Level",
